2df8d49dab176ba5db8f0aac3a6ff87ea6993861
renato76 <renmignogna@gmail.com>
2021-03-05 21:40:52 +0000

    added storecard template, get all stores, and create store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Store = mongoose.model('Store')
 
 exports.homePage = (req, res) => {
-  console.log(req.name)
+  // console.log(req.name)
   res.render('index')
 }
 
@@ -11,9 +11,14 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body)
-  await store.save()
-  res.redirect('/')
+  const store = await(new Store(req.body)).save()
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`)
+  res.redirect(`/store/${store.slug}`)
 }
-
+exports.getStores = async (req, res) => {
+  // Query the database for a list of all stores
+  const stores = await Store.find()
+  console.log(stores)
+  res.render('stores', { title: 'Stores', stores })
+}
 
