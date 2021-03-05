b742fc8e01757ba5a8c5fcaf4d1fe82a1fb7bf98
renato76 <renmignogna@gmail.com>
2021-03-05 22:23:03 +0000

    created updateStore with success message - users can now edit a stores details

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -18,7 +18,29 @@ exports.createStore = async (req, res) => {
 exports.getStores = async (req, res) => {
   // Query the database for a list of all stores
   const stores = await Store.find()
-  console.log(stores)
   res.render('stores', { title: 'Stores', stores })
+}
+
+exports.editStore = async (req, res) => {
+  // 1.find the store given the ID
+  // res.json(req.params)
+  const store = await Store.findOne({ _id: req.params.id })
+  // res.json(store)
+  // 2.confirm they are the owner of the store
+
+  // 3.render out the edit form so the user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store })
+}
+
+exports.updateStore = async (req, res) => {
+  // find and update store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return new store instead of old one
+    runValidators: true
+  }).exec()
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. 
+  <a href="/stores/${store.slug}">View Store</a>`)
+  res.redirect(`/stores/${store._id}/edit`)
+  // redirect them to the store and tell them it worked!
 }
 
