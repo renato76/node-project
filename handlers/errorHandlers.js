a2a98c6df7d9357e35e6ade4c32c32a37c64495e
renato76 <renmignogna@gmail.com>
2021-03-06 22:19:01 +0000

    Geolocation - added lng and lat for each store in storeform

--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -67,9 +67,9 @@ exports.developmentErrors = (err, req, res, next) => {
   No stacktraces are leaked to user
 */
 exports.productionErrors = (err, req, res, next) => {
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
-  });
-};
+  })
+}
