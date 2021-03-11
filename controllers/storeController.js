3334adfb2cbe248e87d2124adf1fb47be4a3a845
renato76 <renmignogna@gmail.com>
2021-03-11 22:48:37 +0000

    tags now filtering correctly, main page shows all stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -85,8 +85,12 @@ exports.getStoreBySlug = async (req, res, next) => {
 
 exports.getStoresByTag = async (req, res) => {
   // res.send('it works!')
-  const tags = await Store.getTagsList()
-  // res.json(tags)
   const tag = req.params.tag
-  res.render('tag', { tags, title: 'Tags', tag } )
+  const tagQuery = tag || { $exists: true }
+  const tagsPromise = Store.getTagsList()
+  const storesPromise = Store.find({ tags: tagQuery })
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise])
+  // res.json(result)
+  // res.json(tags)
+  res.render('tag', { tags, title: 'Tags', tag, stores } )
 }
