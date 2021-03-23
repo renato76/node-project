d3b2589318634e616e2bb5d40e1c104419294a03
renato76 <renmignogna@gmail.com>
2021-03-23 23:23:27 +0000

    added user registration which also features automatic login once they register

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const storeController = require('../controllers/storeController')
 const userController = require('../controllers/userController')
+const authController = require('../controllers/authController')
 const { catchErrors } = require('../handlers/errorHandlers')
 
 router.get('/', catchErrors(storeController.getStores))
@@ -26,5 +27,17 @@ router.get('/tags', catchErrors(storeController.getStoresByTag))
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag))
 
 router.get('/login', userController.loginForm)
+router.get('/register', userController.registerForm)
+
+// 1. validate the registration data
+// 2. register the user
+// 3. log them in
+router.post('/register', 
+  userController.validateRegister,
+  userController.register,
+  authController.login
+)
+
+
 
 module.exports = router
--- start.js
+++ start.js
@@ -8,20 +8,20 @@ if (major < 7 || (major === 7 && minor <= 5)) {
 }
 
 // import environmental variables from our variables.env file
-require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config({ path: 'variables.env' })
 
 // Connect to our Database and handle any bad connections
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.DATABASE)
 mongoose.Promise = global.Promise // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
-  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
-});
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`)
+})
 
 // READY?! Let's go!
 
 // Import all of our models
 require('./models/Store')
-
+require('./models/User')
 
 // Start our app!
 const app = require('./app');
